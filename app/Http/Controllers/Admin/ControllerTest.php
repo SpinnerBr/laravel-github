2ae593ee6facf7755b92bb77e6d0ba9caf008a0a
Thiago <thiago_kuka@hotmail.com>
2022-03-09 16:03:18 -0300

    Amém d

---
+++ app/Http/Controllers/Admin/ControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ControllerTest extends Controller
+{
+    public function teste()
+    {
+        return 'Controller Test';
+    }
+}
